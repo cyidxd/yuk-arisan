e71eed1fc65a92d73112efae10af1a90545f3194
cyidxd <cyidlee133@gmail.com>
2026-06-14 14:40:56 +0700

    Add models __init__

---
+++ app/models/__init__.py
@@ -0,0 +1,13 @@
+from app.models.user import User
+from app.models.arisan import Arisan, ArisanMember
+from app.models.saldo import Saldo
+from app.models.transaksi import Transaksi, Komisi
+
+__all__ = [
+    "User",
+    "Arisan",
+    "ArisanMember",
+    "Saldo",
+    "Transaksi",
+    "Komisi",
+]
